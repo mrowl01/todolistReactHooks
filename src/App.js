2d8c82cab7e1a2469eab87c9302587c9a8d8df8f
Chris O <o@Chriss-MacBook-Pro.local>
2019-05-28 12:53:59 -0500

    used reacthooks to make a todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [text, setText] = useState('');
+  let [list, setList] = useState([]);
+
+  const onChange = (input) =>{
+    setText(input.target.value)
+  }
+  const onSubmit = () =>{
+    if(text==='') return;
+    setList([...list, {
+      value:text,
+      done:false
+    }])
+    setText('');
+    document.querySelector('input').value='';
+  }
+  const done = (i) =>{
+    setList([...list],  list[i].done = !list[i].done )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input onChange = {onChange} />
+      <button onClick= {onSubmit} > Submit </button>
+      <ul>
+        {
+          list.map((item, index) => 
+            <li key={index} 
+            onClick={e => done(index)}
+            className={(list[index].done) ? 'done': null}
+            >
+            {item.value}
+            </li>)
+        }
+      </ul>
     </div>
   );
 }
